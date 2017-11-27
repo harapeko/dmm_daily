c0e5aa9c433dcd75d0e47d562cb0bc2c331c26ba
harapeko <tea.lile.tea@gmail.com>
2017-11-28 00:47:33 +0900

    ポイント交換のタスクを修正した。ミッション達成の受取はやらないようにした。

--- exchange.js
+++ exchange.js
@@ -4,7 +4,7 @@ var system = require('system');
 var URL_FREEGET  = 'http://www.dmm.com/netgame/freeget/';
 var URL_EXCHANGE = 'http://www.dmm.com/netgame/freeget/-/exchange/';
 
-casper.start(URL_FREEGET, function() {
+casper.start(URL_EXCHANGE, function() {
   this.echo('start');
   this.fill('form.login', {
     'login_id': system.env.DMM_ID,
@@ -13,21 +13,10 @@ casper.start(URL_FREEGET, function() {
   this.capture('capture/exchange/start.png');
 });
 
-casper.waitForUrl(URL_FREEGET, function() {
-  this.capture('capture/exchange/logined.png');
-});
-
-casper.then(function() {
-  this.click('.bt-all');
-  this.capture('capture/exchange/bt-all.png');
-
-  this.click('.bt-trade');
-  this.capture('capture/exchange/bt-trade.png');
-});
-
 casper.waitForUrl(URL_EXCHANGE, function() {
   this.capture('capture/exchange/exchange.png');
 
+  // TODO：ボタンが非活性のときは終了する
   this.click('.exchange-once a');
   this.capture('capture/exchange/exchange-once.png');
 

7fc4f0f9a0cd3c40bb789e8bf1ea7200da543678
harapeko <tea.lile.tea@gmail.com>
2017-11-27 23:52:32 +0900

    missionタスク。casper.each()を使うようにした

--- mission.js
+++ mission.js
@@ -3,11 +3,13 @@ var system = require('system');
 
 var DAILY_PACHINKO = 'http://www.dmm.com/netgame/pachinko/-/game/';
 var GAMES_TOP      = 'http://games.dmm.com/';
-var GAMES_1        = 'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=777106/';
-var GAMES_2        = 'http://pc-play.games.dmm.com/play/strike';
-var GAMES_3        = 'http://pc-play.games.dmm.com/play/seiken';
-var GAMES_4        = 'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/';
-var GAMES_5        = 'http://pc-play.games.dmm.com/play/kanpani';
+var GAMES = [
+  'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=777106/',
+  'http://pc-play.games.dmm.com/play/strike',
+  'http://pc-play.games.dmm.com/play/seiken',
+  'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/',
+  'http://pc-play.games.dmm.com/play/kanpani',
+]
 
 // デイリーパチンコ
 // ログインするためにデイリーパチンコから始める(トップだとログインフォームにリダイレクトされないので)
@@ -29,29 +31,11 @@ casper.thenOpen(GAMES_TOP, function() {
   this.capture('capture/mission/GAMES_TOP.png');
 });
 
-// GAME1
-casper.thenOpen(GAMES_1, function() {
-  this.capture('capture/mission/GAMES_1.png');
-});
-
-// GAME2
-casper.thenOpen(GAMES_2, function() {
-  this.capture('capture/mission/GAMES_2.png');
-});
-
-// GAME3
-casper.thenOpen(GAMES_3, function() {
-  this.capture('capture/mission/GAMES_3.png');
-});
-
-// GAME4
-casper.thenOpen(GAMES_4, function() {
-  this.capture('capture/mission/GAMES_4.png');
-});
-
-// GAME5
-casper.thenOpen(GAMES_5, function() {
-  this.capture('capture/mission/GAMES_5.png');
+// 各ゲーム
+casper.each(GAMES, function(self, link) {
+  self.thenOpen(link, function() {
+      this.capture('capture/mission/' + this.getTitle() + '.png');
+  });
 });
 
 casper.run(function () {

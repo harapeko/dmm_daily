de824c1ce069e6f7d396edd5311716f90a4eaa5b
harapeko <github@harapeko.me>
2019-03-29 05:34:14 +0900

    #5 add png, jpg abort for speed up

--- mission.js
+++ mission.js
@@ -19,6 +19,13 @@ const MISSIONS_DEFAULT = [
 void(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  await page.setRequestInterception(true)
+  page.on('request', interceptedRequest => {
+    if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg'))
+      interceptedRequest.abort()
+    else
+      interceptedRequest.continue()
+  });
 
   // ログイン
   await page.goto(LOGIN_PAGE, {waitUntil: 'domcontentloaded'})

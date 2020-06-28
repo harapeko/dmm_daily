73129f610f3de367cb38ab3ad5b8964200abeed1
harapeko <gmail@harapeko.dev>
2020-06-28 21:35:28 +0900

    modify intersectionがあるとログインできないのでコメントアウトした

--- launch.js
+++ launch.js
@@ -6,13 +6,15 @@ module.exports = async () => {
   const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
   await page.setDefaultTimeout(100000)
-  await page.setRequestInterception(true)
-  await page.on('request', interceptedRequest => {
-    if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg'))
-      interceptedRequest.abort()
-    else
-      interceptedRequest.continue()
-  })
+  // 動かなくなってしまった
+  // await page.setRequestInterception(true)
+  // await page.on('request', interceptedRequest => {
+  //   if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg'))
+  //     interceptedRequest.abort()
+  //   else
+  //     interceptedRequest.continue()
+  // })
+
 
   return [browser, page]
 }
